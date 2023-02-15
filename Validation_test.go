f86de12730cde80cce3f62a5139eeb9e0b3fe97f
Hot <hotgabpom@gmail.com>
2023-02-15 10:31:42 +0700

    create Validation - close #2

---
+++ Validation_test.go
@@ -0,0 +1,30 @@
+package exam
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
+)
+
+type Video struct {
+	gorm.Model
+	Name string `valid:"required"`
+	Url  string `valid:"url"`
+}
+
+func TestValidationVideo(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("check Name", func(t *testing.T) {
+		v := Video{
+			Name: "",
+			Url:  "https://www.youtube.com/watch?v=q9wUNgqVK0I",
+		}
+		ok, err := govalidator.ValidateStruct(v)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Name: non zero value required"))
+	})
+}
